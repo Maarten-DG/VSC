f2153766ef0d6a116b1267170c0866d0f3635802
MaartenDG <maarten.de.groote@outlook.com>
2026-01-18 19:06:28 +0100

    Opdracht 1 Implementatie stap 2: mapping uitbreiden

--- Project_1_URL-shortener/main.py
+++ Project_1_URL-shortener/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,29 @@ url_mapping = {
     "google": "https://www.google.com",
     "canvas": "https://ap.instructure.com"
 }
+
+@app.route("/")
+def home():
+    # Input ophalen
+    alias = request.args.get('alias')
+    url = request.args.get('url')
+    errors = []
+
+    # Check of de gebruiker al iets heeft proberen te versturen
+    # (Als alias None is, is de bezoeker er voor de eerste keer)
+    if alias is not None or url is not None:
+        if not alias:
+            errors.append('De alias mag niet leeg zijn')
+        if not url:
+            errors.append("De url mag niet leeg zijn.")
+
+        # Als er geen fouten zijn: opslaan in dict
+        if not errors:
+            url_mapping[alias] = url
+            return render_template("succes.html", alias=alias, url=url)
+        
+    # Als er fouten zijn (of eerste bezoek), toon formulier met eventuele fouten
+    return render_template("home.html", fouten=errors)
 
 @app.route("/shorturl/<alias>")
 def toon_mapping(alias):
